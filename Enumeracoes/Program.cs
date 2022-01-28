4d40e3c0421e16dfdec1521380bdcb1c98344397
Diego Leite <diego@DESKTOP-ME841EH>
2022-01-27 21:26:25 -0300

    teste na classe program

--- Enumeracoes/Program.cs
+++ Enumeracoes/Program.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using Enumeracoes.Entities;
+using Enumeracoes.Entities.Enums;
+using System;
 
 namespace Enumeracoes {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            Order order = new Order {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+            
         }
     }
 }
